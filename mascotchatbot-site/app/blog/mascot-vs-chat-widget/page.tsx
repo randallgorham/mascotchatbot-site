6f8e81721b78c2c88b540a6146949f2d295986e2
randallgorham <randallgorham@gmail.com>
2026-06-22 20:40:10 -0600

    Create page.tsx

---
+++ mascotchatbot-site/app/blog/mascot-vs-chat-widget/page.tsx
@@ -0,0 +1,3 @@
+import { BlogPost, blogMeta } from "@/components/Blog";
+export const metadata = blogMeta("mascot-vs-chat-widget");
+export default function Page() { return <BlogPost slug="mascot-vs-chat-widget" />; }
