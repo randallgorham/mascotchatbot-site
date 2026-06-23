b79962f483dbf1b146360ed88ce1241da2e999aa
randallgorham <randallgorham@gmail.com>
2026-06-22 18:00:48 -0600

    Add files via upload

---
+++ mascotchatbot-site/app/industry/med-spas/page.tsx
@@ -0,0 +1,7 @@
+import IndustryLanding, { industryMeta } from "@/components/IndustryLanding";
+
+export const metadata = industryMeta("med-spas");
+
+export default function Page() {
+  return <IndustryLanding slug="med-spas" />;
+}
